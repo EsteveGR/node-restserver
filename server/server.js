1a2dea76dcfb427f73ca1db8314cfefc059f4871
Evetse <egrassot@gmail.com>
2019-04-06 16:27:17 +0200

    mongoose configuration

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 require ('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
@@ -42,6 +46,14 @@ app.put('/usuario/:id', function (req, res) {
 
 app.delete('/usuario', function (req, res) {
     res.json('delete usuario');
+});
+
+
+mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true}, (err,res) =>{
+
+    if (err) throw err;
+ 
+    console.log('Mongo ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
